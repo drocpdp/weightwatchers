24dc7caf7554f4e8eae3aae07b0bc1d63ee4d934
David Eynon <davideynon@Davids-MacBook-Pro.local>
2021-06-10 14:50:55 +0200

    simplified further for now, eliminating people, and simplifying json data

--- data.js
+++ data.js
@@ -1,36 +1,15 @@
 const weights = [
     
     {
-        id: 1,
-        weight: 290,
-        datetime: 1623314780021,
+        weight: 290
     },
     {
-        id: 2,
-        weight: 287,
-        datetime: 1623314791143,
+        weight: 287
     },
     {
-        id: 3,
-        weight: 256,
-        datetime: 1623314791143,
+        weight: 256
     }        
 
 ]
 
-const people = [
-
-    {
-        id: 1,
-        fname: "David",
-        lname: "Eynon"
-    },
-    {
-        id: 2,
-        fname: "Donald",
-        lname: "Evron"
-    }    
-
-]
-
-module.exports = {weights, people}
+module.exports = {weights}
